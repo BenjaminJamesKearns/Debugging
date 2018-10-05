3d2a2c5379da12295227b205a75d38a3a3c3b302
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-05 08:35:19 +0000

    debugged  exercise 1

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,3 +1,10 @@
+=begin
+#before
+def say_hello(name)
+  "hello #(name)" #always puts "hello #(name) as the syntax is wrong so it doesn't use the name argument
+end
+=end
+#after
 def say_hello(name)
   "hello #{name}"
 end
