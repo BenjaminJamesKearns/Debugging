5711f0b6d92c8456325185d00e4a7c14b242cf05
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-08 10:34:01 +0000

    worked on exercise 2

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,23 +1,23 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
-    (65 + cipher.find_index(char)).chr
+    (65 + cipher.find_index(char).to_i).chr  # added to to_i as it needs to be an integer to work with +.
   end
-  ciphertext_chars.join
+  return ciphertext_chars.join
 end
 
 def decode(ciphertext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[char.ord - 65] # moved the 65 and char.ord around to get the correct output
   end
+  puts plaintext_chars 
   plaintext_chars.join
 end
-
 # Intended output:
 #
 # > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
 # => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
 #
-# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+#decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
