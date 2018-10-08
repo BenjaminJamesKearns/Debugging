f26e9c10f55f183bfb6d90112495a969271f40bc
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-08 15:34:09 +0000

    finished exercise 2

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,10 +1,10 @@
 =begin
 #before
 def say_hello(name)
-  "hello #(name)"  # always puts "hello #(name)" as the syntax is wrong so it doesn't use the name argument
+  "hello #(name)"  
 end
 =end
 #after
 def say_hello(name)
-  "hello #{name}"
+  "hello #{name}" # always puts "hello #(name)" as the syntax is wrong so it doesn't use the name argument
 end
--- exercise_2.rb
+++ exercise_2.rb
@@ -1,5 +1,24 @@
+=begin
+#before
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+  end
+  ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[65 - char.ord]
+  end
+  plaintext_chars.join
+end
+=end
+#after
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars) # needs to be 2 dots and not 3 to go from 'a' to 'z'
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char).to_i).chr  # added to to_i as it needs to be an integer to work with +.
   end
@@ -7,7 +26,7 @@ def encode(plaintext, key)
 end
 
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars) # needs to be 2 dots and not 3 to go from 'a' to 'z'
   plaintext_chars = ciphertext.chars.map do |char|
     cipher[char.ord - 65] # moved the 65 and char.ord around to get the correct output
   end
