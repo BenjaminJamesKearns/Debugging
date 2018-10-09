4da4217d29c6b2a7fd31e4cdc17560b9d32d96e5
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-09 08:20:48 +0000

    removed some comments

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,10 +1,3 @@
-=begin
-#before
-def say_hello(name)
-  "hello #(name)"  
-end
-=end
-#after
 def say_hello(name)
   "hello #{name}" # always puts "hello #(name)" as the syntax is wrong so it doesn't use the name argument
 end
--- exercise_2.rb
+++ exercise_2.rb
@@ -1,22 +1,3 @@
-=begin
-#before
-def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-  ciphertext_chars = plaintext.chars.map do |char|
-    (65 + cipher.find_index(char)).chr
-  end
-  ciphertext_chars.join
-end
-
-def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-  plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
-  end
-  plaintext_chars.join
-end
-=end
-#after
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars) # needs to be 2 dots and not 3 to go from 'a' to 'z'
   ciphertext_chars = plaintext.chars.map do |char|
